77feca755719549903e0fa0def3dbdf6dfbcec83
articuly <articuly@gmail.com>
2020-03-21 10:39:14 +0800

    修改部分模板，调整视图

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.blog_title, name='blog_title'),
+    path('', views.blog_home, name='blog_home'),
     path('<int:article_id>/', views.blog_article, name='blog_article')
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from .models import BlogArticles
 
 
-def blog_title(request):
+def blog_home(request):
     blogs = BlogArticles.objects.all()
-    return render(request, 'blog/titles.html', {'blogs': blogs})
+    return render(request, 'blog/home.html', {'blogs': blogs})
 
 
 def blog_article(request, article_id):
     article = BlogArticles.objects.get(id=article_id)
     pub = article.publish
-    return render(request, 'blog/content.html', {'article': article, 'publish': pub})
+    return render(request, 'blog/article.html', {'article': article, 'publish': pub})
