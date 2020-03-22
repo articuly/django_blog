6471a5599bd0cd2d696f22b0ce6403dc7d1900ad
articuly <articuly@gmail.com>
2020-03-22 20:09:47 +0800

    course应用，增加view, url

--- course/urls.py
+++ course/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from django.views.generic import TemplateView
-from .views import CourseHome
+from .views import CourseHome, CourseListView
 
 app_name = 'course'
 
 urlpatterns = [
     path('', CourseHome.as_view()),
+    path('course-list', CourseListView.as_view(), name='course_list')
 ]
--- course/views.py
+++ course/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Course
 
 
 class CourseHome(TemplateView):
     template_name = 'course/home.html'
+
+
+class CourseListView(ListView):
+    model = Course  # Course.objects.all()
+    template_name = 'course/course_list.html'
+    context_object_name = 'course'
